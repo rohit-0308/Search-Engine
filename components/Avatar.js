fd58dc331e193d4cb8ae34160fcc1fa4df66273b
rohit-0308 <rohitkumar860311@gmail.com>
2021-05-06 15:01:08 +0530

    Google Front Page UI Completed

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,5 @@
 function Avatar({ url }) {
-  return <img loading="lazy" className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110" src={url} alt="Profile Picture" />;
+  return <img loading="lazy" className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 " src={url} alt="Profile Picture" />;
 }
 
 export default Avatar;
---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <p>India</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
